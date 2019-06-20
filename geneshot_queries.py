0b5745c5fd05774a4289c18965fcbe1610b3aaa1
Allison Seiden <ahseiden@gmail.com>
2019-06-20 11:51:41 -0400

    update to read in desired files in data folder

--- geneshot_queries.py
+++ geneshot_queries.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import requests
 
-UK_biobank = pd.read_excel("~/Downloads/UK_biobank.xlsx", sheet_name = 1)
-phenotypes = UK_biobank['Phenotype Description'].tolist()
-phenotypes = list(dict.fromkeys(phenotypes))
+cancers = pd.read_csv("cancer_phenotypes.txt", header=None, names=['ID', "Description"])
+noncancer_illnesses = pd.read_csv("noncancerillness_phenotypes.txt", header=None, names=['ID', "Description"])
+medications = pd.read_csv("medication_phenotypes.txt", header=None, names=['ID', "Description"])
+mental_illnesses = pd.read_csv("mentalillness_phenotypes.txt", header=None, names=['ID', "Description"])
