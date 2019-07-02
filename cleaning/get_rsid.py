424520cec28aa7d962df025df877381ac8a4f063
Allison Seiden <ahseiden@gmail.com>
2019-07-02 12:11:27 -0400

    added command line functionality

--- cleaning/get_rsid.py
+++ cleaning/get_rsid.py
@@ -9,15 +9,30 @@ def reformat(snp):
     new_str = 'chr' + chrom + '\t' + str(pos) + '\t' + str(pos)
     return new_str
 
-def run(file_list):
-    for file in file_list:
-        genes_df = pd.read_csv(file, sep='\t')
+def run(file_list, outfiles):
+    if len(file_list) != len(outfiles):
+        print("Error: length of input file and output file do not match")
+        return
+    for i in range(len(file_list)):
+        genes_df = pd.read_csv(file_list[i], sep='\t')
         SNP_loc = genes_df.copy().SNP_location
         SNP_loc.drop_duplicates(keep='first', inplace=True)
 
         for_cmd = SNP_loc.apply(reformat)
         for_cmd.to_csv('coord.txt', index=False, header=False)
 
-        outfile = file[file.rfind('/')+1:file.rfind('_')] + '_rsid.tsv'
-        cmd = 'awk \'{gsub(\"chr\",\"\",$1)} {print $1\":\"$2\"-\"$3}\' coord.txt | parallel  --delay 1 tabix ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh37p13/VCF/All_20180423.vcf.gz {} | awk -v OFS=\"\t\" \'BEGIN {print \"chromosome\",\"Coordinate\",\"rsID\",\"Ref\",\"Alt\"} {print \"chr\"$1,$2,$3,$4,$5}\' > ' + outfile
+        cmd = 'awk \'{gsub(\"chr\",\"\",$1)} {print $1\":\"$2\"-\"$3}\' coord.txt | parallel  --delay 1 tabix ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh37p13/VCF/All_20180423.vcf.gz {} | awk -v OFS=\"\t\" \'BEGIN {print \"chromosome\",\"Coordinate\",\"rsID\",\"Ref\",\"Alt\"} {print \"chr\"$1,$2,$3,$4,$5}\' > ' + outfiles[i]
         sp.call(cmd, shell=True)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='input file with filenames on each line, each file should have SNPs in column named SNP_location formatted chr:pos')
+    parser.add_argument('-o', '--output', default=sys.stdout, help='file with names of output file names, should match input file')
+    args = parser.parse_args()
+    with open(args.input) as f:
+        filenames = f.readlines()
+    filenames = [x.strip() for x in filenames]
+    with open(args.output) as f:
+        outfiles = f.readlines()
+    outfiles = [x.strip() for x in outfiles]
+    run(filenames, outfiles)
