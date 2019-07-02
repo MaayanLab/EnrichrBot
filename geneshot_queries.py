b5dc8db10b3d82a9dd0fa457aea8864341de6de0
Allison Seiden <ahseiden@gmail.com>
2019-07-02 12:26:22 -0400

    minor changes, organization

--- geneshot_queries.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-import requests
-
-cancers = pd.read_csv("cancer_phenotypes.txt", header=None, names=['ID', "Description"])
-noncancer_illnesses = pd.read_csv("noncancerillness_phenotypes.txt", header=None, names=['ID', "Description"])
-medications = pd.read_csv("medication_phenotypes.txt", header=None, names=['ID', "Description"])
-mental_illnesses = pd.read_csv("mentalillness_phenotypes.txt", header=None, names=['ID', "Description"])
