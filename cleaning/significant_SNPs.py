33cc906432b307daa499056f581640fc245b2de3
Allison Seiden <ahseiden@gmail.com>
2019-07-17 10:46:09 -0400

    pulls significant SNPs from original file, uses command line to specify files and pvalue cutoff

--- cleaning/significant_SNPs.py
+++ cleaning/significant_SNPs.py
@@ -1,82 +1,98 @@
-""" Pull significant SNPs out of UK Biobank files
-    Adjust p-values in the file using Benjamini-Hochberg and Bonferroni
-    corrections
-    Choose SNPs that have a signigicant p-value after adjusting using either
-    the BH or Bonferroni methods
-"""
-
 import pandas as pd
-import gzip
-from rpy2.robjects.packages import importr
-from rpy2.robjects.vectors import FloatVector
-import subprocess as sp
+import numpy as np
 import multiprocessing as mp
-import matplotlib.pyplot as plt
+import argparse
+import sys
+import gzip
 
-
-# read in gzipped data file from UK biobank and mark that file with the
-# appropriate sex
-def read_file(filename):
+"""
+    pull out SNPs with p-value less than cutoff parameter,
+    also removes any SNPs that have low_confidence_variant = True
+"""
+def get_significant(filename, cutoff):
     with gzip.open(filename) as f:
-        data = pd.read_csv(f, sep='\t')
+        df = pd.read_csv(f, sep='\t')
+    df = df.loc[df.pval < cutoff]
+    df = df.loc[df['low_confidence_variant'] == False]
+    return df
+
+"""
+    Reformat variant column from chr:pos:ref:alt into 4 separate columns
+    Sort by p-value and reset the index
+"""
+def reformat_data(data):
+    if data.shape[0] == 0:
+        return data
+    var_split = data["variant"].str.split(":", expand=True)
+    data["chrom"] = var_split[0]
+    data["pos"] = var_split[1].astype('int64')
+    data["ref"] = var_split[2]
+    data["alt"] = var_split[3]
+    data.drop(columns = ["variant"], inplace = True)
+    cols = data.columns.tolist()
+    cols = cols[-4:] + cols[:-4]
+    data = data[cols]
+
+    data.sort_values(by=['pval'], inplace=True)
+    data.reset_index(inplace=True)
+    data.drop(columns=['index'], inplace=True)
+
+    return data
+
+"""
+    Eliminate any SNPs within 500kbp of one another
+    Works for a single chromosome
+"""
+def per_chrom(data):
+    df = data.copy()
+    i = 0
+    while i < df.shape[0]:
+        curr_snp = df.iloc[i]
+        lower = curr_snp.pos - 500000
+        upper = curr_snp.pos + 500000
+        df = df.loc[(df.pos < lower) | (df.pos > upper) | (df.pos == curr_snp.pos)]
+        df.reset_index(inplace=True)
+        df.drop(columns=['index'], inplace=True)
+        i += 1
+    return df
+
+"""
+    Run the method to pick out SNPs for every chromosome in the file
+"""
+def all_chroms(big_df):
+    by_chrom = big_df.groupby(['chrom'])
+    dfs = []
+    for chrom, snps in by_chrom:
+        dfs.append(per_chrom(snps))
+    final_df = pd.concat(dfs, sort=False)
+    return final_df
+
+"""
+    Put everything together to get the file of significant SNPs
+"""
+def get_snps(filename, outfile, cutoff):
     indices = [pos for pos, char in enumerate(filename) if char == '.']
-    data.loc[:,"Sex"] = filename[indices[2]+1:indices[3]]
-    return data
+    ID = filename[:indices[0]] + filename[indices[2]:indices[-2]]
+    print("running ", ID)
+    my_data = get_significant(filename, cutoff)
+    if my_data.shape[0] == 0:
+        my_data.to_csv(outfile, index=False, header=True, sep='\t')
+        return
+    my_data = reformat_data(my_data)
+    final_data = all_chroms(my_data)
+    if outfile == "print":
+        print(final_data)
+    else:
+        final_data.to_csv(outfile, index=False, header=True, sep='\t')
 
-# combine dataframes with the same phenotype code for simplicity
-def combine_sameID(ID):
-    substr = str(ID) + ".gwas.imputed_v3"
-    files = [x for x in filenames if x.startswith(substr)]
-    dataframes = []
-    for file in files:
-        dataframes.append(read_file(file))
-    complete_df = pd.concat(dataframes)
-    return complete_df
-
-# adjust the p-values in the data using the Benjamini-Hochberg and Bonferroni
-# corrections
-def adjust_pvalue(data):
-    stats = importr('stats')
-    p_adjustBH = stats.p_adjust(FloatVector(data.pval.tolist()), method = 'BH')
-    data["BH"] = p_adjustBH
-    p_adjustBonferroni = stats.p_adjust(FloatVector(data.pval.tolist()), method = 'bonferroni')
-    data["Bonferroni"] = p_adjustBonferroni
-    return data
-
-# collect SNPs that had a significant p-value under either the BH correction
-# or the Bonferroni correction
-def get_significantSNPs(data):
-    signif_SNPs = data.loc[(data["BH"] < 0.05) | (data["Bonferroni"] < 0.05)]
-    return signif_SNPs
-
-# write the significant SNPs and their associated information to a new datafile
-# that will be easier to work with during analysis
-def write(dataframe, ID):
-    outfile = "sigSNPs/" + str(ID) + "_signifSNPs.csv"
-    dataframe.to_csv(outfile, sep='\t', index=False)
-
-# put all smaller steps together to run on each ID, return the number of
-# significant SNPs in the file for density plot
-def run(ID):
-    print("running ", str(ID))
-    data = combine_sameID(ID)
-    data = adjust_pvalue(data)
-    significant_SNPs = get_significantSNPs(data)
-    write(significant_SNPs, ID)
-    return significant_SNPs.shape[0]
-
-# get list of IDs from excel file
-UK_biobank = pd.read_excel("~/Downloads/UK_biobank.xlsx", sheet_name = 1)
-filenames = UK_biobank.File.tolist()
-already_downloaded = pd.read_csv("alreadydownloaded4.txt", header=None)
-already_downloaded = [x[:x.rfind('_')] for x in already_downloaded[0].tolist()]
-IDs = UK_biobank["Phenotype Code"].tolist()
-IDs = IDs[22:]
-IDs = [x for x in IDs if "irnt" not in str(x)]
-IDs = list(dict.fromkeys(IDs))
-IDs = [x for x in IDs if str(x) not in already_downloaded]
 
 if __name__ == '__main__':
-    pool = mp.Pool(processes=3)
-    num_SNPs = pool.map(run, IDs)
-    pd.Series(num_SNPs).to_csv("numSNPs.csv", header=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='input file containing SNPs, column names identical to UK Biobank GWAS')
+    parser.add_argument('-o', '--output', default=sys.stdout, help='name the output file, default will print to screen')
+    parser.add_argument('-p', '--pval', type=float, help='the p-value to use for choosing significant SNPs')
+    args = parser.parse_args()
+    if args.output:
+        get_snps(args.input, args.output, args.pval)
+    else:
+        get_snps(args.input, 'print', args.pval)
