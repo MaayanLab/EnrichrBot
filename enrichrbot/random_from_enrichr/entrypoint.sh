e15e4ed3eaf475d66dc7f77c3249c953b5600eb7
Daniel J. B. Clarke <u8sand@gmail.com>
2019-08-19 15:59:02 -0400

    [enrichrbot/random_from_enrichr]: Minor verbosity improvements to cron

--- enrichrbot/random_from_enrichr/entrypoint.sh
+++ enrichrbot/random_from_enrichr/entrypoint.sh
@@ -14,11 +14,12 @@ if [ "$1" == "bash" ]; then
   bash
 elif [ "$1" == "cron" ] || [ "${FORCE_CRON}" -eq 1 ]; then
   echo "Running in cron mode..."
+  date
   set -x
   (
-    echo "${CRON} sudo -u ${USER} /app/enrichrbot.py >> /proc/1/fd/1 2>/proc/1/fd/2"
+    echo "${CRON} (date && cd /app/ && sudo -u ${USER} /app/enrichrbot.py) >> /proc/1/fd/1 2>/proc/1/fd/2"
   ) >> /etc/cron.d/enrichrbot
-  chmod +x /etc/cron.d/enrichrbot
+  chmod 700 /etc/cron.d/enrichrbot
   crontab /etc/cron.d/enrichrbot
   cron -f
 else
