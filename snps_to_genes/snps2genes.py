502ed4c835de6f83e7a0bb21edf5aa4f22dfa49a
Alon <bartala@gmail.com>
2019-09-16 13:32:32 -0400

    Improved call to R biomart

--- snps_to_genes/snps2genes.py
+++ snps_to_genes/snps2genes.py
@@ -7,21 +7,33 @@ import time
 from rpy2.robjects.vectors import StrVector
 import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
-pandas2ri.activate()
 from rpy2.robjects import r as R
 from rpy2.robjects.conversion import localconverter
 from rpy2.robjects.packages import importr
+import os, os.path
+from os import listdir
+from os.path import isfile, join
+import re
+
+pandas2ri.activate()
 
 """
     Read in original GWAS file from UK biobank and pull out SNPs that have
     p-value less than specified cutoff value
 """
+
 def get_significant(filename, pval_cutoff):
-    with gzip.open(filename) as f:
-        df = pd.read_csv(f, sep='\t')
-    df = df.loc[df.pval < pval_cutoff]
-    df = df.loc[df['low_confidence_variant'] == False]
-    return df
+    try:
+        df = pd.read_csv(filename, compression='gzip', header=0, sep='\t', quotechar='"')
+        if 'pval' not in df.columns:
+            return pd.DataFrame(columns=['empty_dataframe'])
+        df = df.loc[df.pval < pval_cutoff]
+        df = df.loc[df['low_confidence_variant'] == False]
+        return df
+    except Exception as e:
+        print(e)
+        return pd.DataFrame(columns=['empty_dataframe'])
+        
 
 """
     Reformat variant column from chr:pos:ref:alt into 4 separate columns
@@ -108,60 +120,77 @@ def per_chromosome(data, chr_num, ucutoff, dcutoff):
     del(hg19_chrom)
     return df
 
-def get_geneset(genes, outfile):
+def get_geneset(genes, outfile, mart):
     if genes.shape[0] == 0:
         genes.to_csv(outfile, sep='\t')
         return
     with localconverter(ro.default_converter + pandas2ri.converter):
         t_IDs = ro.conversion.py2rpy(genes['gene_ID'])
     R.library("biomaRt")
-    #mart = R.useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl", host="http://grch37.ensembl.org")
-    flag = True
-    while flag:
-        try:
-            mart = R.useMart(biomart="ENSEMBL_MART_ENSEMBL",dataset="hsapiens_gene_ensembl", host="www.ensembl.org")
-            flag = False
-        except:
-            print("Faild to connect to Ensembl, trying again in 5sec.")
-            time.sleep(5)
-    genes = R.getBM(attributes = StrVector(("ensembl_gene_id", "hgnc_symbol")),
-                    filters = "ensembl_gene_id",
-                    values = t_IDs,
-                    mart = mart)
-    utils = importr("utils")
-    utils.write_table(genes['hgnc_symbol'], file = outfile, append=False, row_names = False, col_names = False, quote = False)
+    try:
+        genes = R.getBM(attributes = StrVector(("ensembl_gene_id", "hgnc_symbol")),
+                        filters = "ensembl_gene_id",
+                        values = t_IDs,
+                        mart = mart)
+        genes.to_csv(outfile+".gz",index=False,compression='gzip')
+    except:
+        print("error connecting to BioMart: ", outfile)
+        pass
+
 
 """
     compile all smaller functions to run for single file
 """
-def run(filename, ucutoff, dcutoff, pval, outfile):
+def run(filename, ucutoff, dcutoff, pval, outfile, mart):
     snps = get_snps(filename, pval)
     if snps.shape[0] == 0:
+        print("empty dataframe")
         return snps
     by_chrom = snps.groupby("chrom")
     dataframes = []
     for chr, SNPs in by_chrom:
         dataframes.append(per_chromosome(SNPs, chr, ucutoff, dcutoff))
     nearest_genes = pd.concat(dataframes, sort=False)
-    get_geneset(nearest_genes, outfile)
+    get_geneset(nearest_genes, outfile, mart)
 
 """
     read in hg19 assembly, downloaded from UCSC Table Browser
     rename the columns to be more readable
     only keep protein coding genes
 """
-pth= "/users/alon/desktop/enrichrbot-master/snps_to_genes/data/hg19assembly.dms"
+pth= "/users/alon/desktop/github/enrichrbot/snps_to_genes/data/hg19assembly.dms"
 hg19 = pd.read_csv(pth, sep='\t', header=0, names=["bin", "name", "chrom", "strand", "txStart", "txEnd",
 "cdsStart", "cdsEnd", "exonCount", "exonStarts", "exonEnds", "score", "gene_ID", "cdsStartStat", "cdsEndStat", "exonFrames", "source"])
 hg19 = hg19.loc[hg19["source"] == "protein_coding"]
 
+# create biomaRt object
+R.library("biomaRt")
+mart = R.useMart(biomart="ENSEMBL_MART_ENSEMBL",dataset="hsapiens_gene_ensembl", host="www.ensembl.org")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help='input GWAS file from UK Biobank')
-    parser.add_argument('-p', '--pval', type=float, default=5e-8, help='specify p-value cutoff for SNPs, default is 5e-8')
-    parser.add_argument('-u', '--upcutoff', type=int, default=np.inf, help='specify the upstream cutoff distance, default is no cutoff')
-    parser.add_argument('-d', '--downcutoff', type=int, default=np.inf, help='specify the downstream cutoff distance, default is no cutoff')
-    parser.add_argument('-o', '--output', help='name the output file')
-    args = parser.parse_args()
-    genes = run(args.input, args.upcutoff, args.downcutoff, args.pval, args.output)
+
+# get list of files
+mypath = r"/Volumes/My Book/"
+onlyfiles = [f for f in os.listdir(mypath+"AHS_projectdata") if not f.startswith('.')] # ignore hidden files
+
+# analyze all files
+# upsteream = downstream = 100,000
+# p-value = 5e-8
+i = 0
+for file in onlyfiles:
+    run(os.path.join(mypath,"AHS_projectdata",file), 100000, 100000, 5e-8, os.path.join(mypath,"out",file+".csv"), mart)
+    print(i)
+    i=i+1
+
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('-i', '--input', help='input GWAS file from UK Biobank')
+#     parser.add_argument('-p', '--pval', type=float, default=5e-8, help='specify p-value cutoff for SNPs, default is 5e-8')
+#     parser.add_argument('-u', '--upcutoff', type=int, default=np.inf, help='specify the upstream cutoff distance, default is no cutoff')
+#     parser.add_argument('-d', '--downcutoff', type=int, default=np.inf, help='specify the downstream cutoff distance, default is no cutoff')
+#     parser.add_argument('-o', '--output', help='name the output file')
+#     args = parser.parse_args()
+#     genes = run(args.input, args.upcutoff, args.downcutoff, args.pval, args.output)
+
+
+
