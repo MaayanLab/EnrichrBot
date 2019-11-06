8aee2b7ee97426aeb1600dc37485a63dedd7043d
Alon <bartala@gmail.com>
2019-11-06 15:51:56 -0500

    Update entrypoint.sh

--- Talk_Back/entrypoint.sh
+++ Talk_Back/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ! pgrep -f QA.py
 then
-  echo "restarted the talkback bot">>/home/maayanlab/enrichrbot/QA/test1.log
+  echo "restarted the talkback bot" $(date)>>/home/maayanlab/enrichrbot/QA/test1.log
   python3 /home/maayanlab/enrichrbot/QA/QA.py
 fi
