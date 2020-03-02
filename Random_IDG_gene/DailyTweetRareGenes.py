366a1367783337884a330dfead10c72d3871419a
Alon <bartala@gmail.com>
2020-03-02 14:52:42 -0500

    Update DailyTweetRareGenes.py

--- Random_IDG_gene/DailyTweetRareGenes.py
+++ Random_IDG_gene/DailyTweetRareGenes.py
@@ -30,6 +30,7 @@ def init_selenium(CHROMEDRIVER_PATH, windowSize='1080,1080'):
   options.add_argument('--disable-extensions')
   options.add_argument('--dns-prefetch-disable')
   options.add_argument('--disable-gpu')
+  options.add_argument('--hide-scrollbars')
   options.add_argument('--window-size={}'.format(windowSize))
   driver = webdriver.Chrome(
     executable_path=CHROMEDRIVER_PATH,
@@ -38,21 +39,9 @@ def init_selenium(CHROMEDRIVER_PATH, windowSize='1080,1080'):
   return driver
 
 
-def is_empty_content(link):
-  resp=requests.get(link) 
-  if resp.status_code==200: 
-    soup = BeautifulSoup(resp.text,'html.parser') 
-    data = soup.find("div",{"id":"missing information"})
-  else: 
-    print("Error website is down")
-  return(data is not None)
-
-
 # Go to the link and take a screenshot
 def link_to_screenshot(link=None, output=None, zoom='100 %', browser=None):
   print('Capturing screenshot...')
-  if is_empty_content(link):
-    return None
   browser.get(link)
   time.sleep(5)
   browser.execute_script("document.body.style.zoom='{}'".format(zoom))
@@ -60,14 +49,6 @@ def link_to_screenshot(link=None, output=None, zoom='100 %', browser=None):
   os.makedirs(os.path.dirname(output), exist_ok=True)
   browser.save_screenshot(output)
   return output
-
-  
-def sample_rows(filename, s=1):
-  # s is the desired sample size
-  n = sum(1 for line in open(filename)) - 1 #number of records in file (excludes header)
-  skip = sorted(random.sample(range(1,n+1),n-s)) #the 0-indexed header will not be included in the skip list
-  df = pd.read_csv(filename, skiprows=skip, sep='\t')
-  return(df.index[0])
 
   
 def main_random_lncRNA():
@@ -80,7 +61,7 @@ def main_random_lncRNA():
   lncRNA = random.choice(lncRNA)
   lnc_link = 'https://amp.pharm.mssm.edu/lnchub/?lnc=' + lncRNA
   # init browser
-  browser = init_selenium(CHROMEDRIVER_PATH, windowSize='1600,1200')
+  browser = init_selenium(CHROMEDRIVER_PATH, windowSize='1100,1000')
   # create and save screenshots
   screenshots = [
     link_to_screenshot( link=lnc_link, output=os.path.join(PTH, "screenshots", "lncRNA.png"), browser=browser, zoom='0.75'),
@@ -88,13 +69,12 @@ def main_random_lncRNA():
   browser.quit()
   # Construct the tweet
   if screenshots[0] is None:
-    screenshots = [i for i in screenshots if i]
-    message ="Explore prior knowledge & functional predictions for the understudied lncRNA {} using #recount2 resource.\n\n{}\n\n{}"
-    message = message.format(lnc_go_bioprocess,lnc_link,"@MaayanLab")
+    print("no screenshot found")
+    return
     # Send the tweet with photos
   else:
-    message ="Explore prior knowledge & functional predictions for the understudied {} {} using #IDG resources:\n\n{}\n\n{}\n\n{}\n\n{}\n\n{}"
-    message = message.format(df[df.Gene==gene].iloc[0][1],gene,archs4_link,harmonizome_link,geneshot_link,pharos_link,"@MaayanLab @DruggableGenome @IDG_Pharos @BD2KLINCSDCIC")
+    message ="Explore prior knowledge & functional predictions for the understudied lncRNA {} with {} using #recount2 resource.\n\n{}"
+    message = message.format(lncRNA,"@MaayanLab",lnc_link)
   # Send the tweet with photos
   auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
   auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -105,7 +85,11 @@ def main_random_lncRNA():
     print('tweet: {} {}'.format(message, media_ids))
   else:
     api.update_status(media_ids=media_ids, status=message)
-
+  # delete screeenshot
+  try:
+    os.remove(os.path.join(PTH, "screenshots", "lncRNA.png"))
+  except:
+    print("failed to delete screenshot")
 
 if __name__ == '__main__':
   main_random_lncRNA()
